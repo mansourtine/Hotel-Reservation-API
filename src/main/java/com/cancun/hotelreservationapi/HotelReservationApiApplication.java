1166ca18b289ca29fc9198d2ca2732c4f4e65980
Mansour TINE <mansourtine@gmail.com>
2022-05-01 12:59:45 +0000

    Initial project version

---
+++ src/main/java/com/cancun/hotelreservationapi/HotelReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.cancun.hotelreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelReservationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cancun/hotelreservationapi/HotelReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cancun.hotelreservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
